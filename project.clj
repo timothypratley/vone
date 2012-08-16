3983215c5586d55be73059dc4acb043286235b7f
Timothy Pratley <timothypratley@gmail.com>
2012-08-15 18:47:56 -0700

    initial creation

---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject vone "0.1.0-SNAPSHOT"
+            :description "FIXME: write this!"
+            :dependencies [[org.clojure/clojure "1.3.0"]
+                           [noir "1.2.1"]
+                           [clj-http "0.5.3"]
+                           [clj-time "0.4.4"]]
+            :main vone.server)
+
