30c22f1e1ad28a70682e2e13468f9352e02f2462
Timothy Pratley <timothypratley@gmail.com>
2013-08-29 16:16:42 -0700

    switching to lein 2.0 plugins syntax

--- project.clj
+++ project.clj
@@ -1,14 +1,14 @@
 (defproject vone "0.1.0-SNAPSHOT"
-            :description "VersionOne Reporting"
-            :dependencies [[org.clojure/clojure "1.4.0"]
-                           [noir "1.3.0"]
-                           [cheshire "5.2.0"]
-                           [clj-http-lite "0.2.0"]
-                           [slingshot "0.10.3"]
-                           [clj-time "0.5.1"]
-                           [org.clojure/data.csv "0.1.2"]]
-            :dev-dependencies [[appengine-magic "0.5.0"]
-                               [lein-ring "0.8.6"]]
-            :ring {:handler vone.server/handler}
-            :main vone.server)
+  :description "VersionOne Reporting"
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [noir "1.3.0"]
+                 [cheshire "5.2.0"]
+                 [clj-http-lite "0.2.0"]
+                 [slingshot "0.10.3"]
+                 [clj-time "0.5.1"]
+                 [org.clojure/data.csv "0.1.2"]]
+  :plugins [[appengine-magic "0.5.0"]
+            [lein-ring "0.8.6"]]
+  :ring {:handler vone.server/handler}
+  :main vone.server)
 
